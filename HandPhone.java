7fce3c17bd8bd73fac2772dc8d431bc4742028f1
larz281 <114352578+larz281@users.noreply.github.com>
2022-10-15 10:53:40 +0700

    Add files via upload

---
+++ HandPhone.java
@@ -0,0 +1,36 @@
+//Teguh Gumelar
+//20210040047
+//TI21A
+//Pemrograman Berorientasi Objek sesi 2 no.4
+
+
+public class HandPhone {
+    String jenis_hp;
+    int tahun_pembuatan;
+    
+    public void setDataHP(String jenis_hp, int tahun_pembuatan){
+        this.jenis_hp = jenis_hp;
+        this.tahun_pembuatan = tahun_pembuatan;
+        
+    }
+    
+    public String getJenisHp(){
+        return jenis_hp;
+    }
+    
+    public int getTahunPembuatan(){
+        return tahun_pembuatan;
+    }
+    
+    
+    public static void main(String[] args) {
+        HandPhone hp = new HandPhone();
+        
+        hp.setDataHP("Nokia", 2020);
+        
+        System.out.println(hp.getJenisHp());
+        System.out.println(hp.getTahunPembuatan());
+        
+        
+    }
+}
